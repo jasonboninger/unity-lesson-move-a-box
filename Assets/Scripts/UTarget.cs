d668ed38a1f39f591aa0837eeb7f1dc6499977fe
Jason Boninger <jason.boninger@gmail.com>
2020-10-09 17:40:57 -0400

    Add hovering targets.

--- Assets/Scripts/UTarget.cs
+++ Assets/Scripts/UTarget.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +10,26 @@ namespace Assets.Scripts
 {
 	public class UTarget : MonoBehaviour
 	{
+		protected void Start()
+		{
+			StartCoroutine(_Hover());
+		}
 
+		private IEnumerator _Hover()
+		{
+			var y = transform.position.y;
+			var value = 0f;
 
+			while (true)
+			{
+				var position = transform.position;
+				value += Time.deltaTime;
+				position.y = y + Mathf.Sin(value) * 0.5f;
+				transform.position = position;
 
+				yield return null;
+
+			}
+		}
 	}
 }
